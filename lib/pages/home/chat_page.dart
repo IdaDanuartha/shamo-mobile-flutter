c35b89e4e6de71e07bb79171574e61495836a3d6
IdaDanuartha <danuart21@gmail.com>
2024-02-07 22:54:31 +0800

    chat page (empty chat)

--- lib/pages/home/chat_page.dart
+++ lib/pages/home/chat_page.dart
@@ -1,14 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_flutter/themes.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
 
+  Widget header() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 10),
+      child: AppBar(
+        backgroundColor: bgColor1,
+        centerTitle: true,
+        title: Text(
+          "Message Support",
+          style: primaryTextStyle.copyWith(
+            fontSize: 18,
+            fontWeight: medium
+          ),
+        ),
+        elevation: 0,
+        automaticallyImplyLeading: false,
+      ),
+    );
+  }
+
+  Widget content() {
+    return Expanded(
+      child: Container(
+        width: double.infinity,        
+        color: bgColor3,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/icon_headset.png", width: 80),
+            SizedBox(height: 20),
+            Text("Oops no message yet?", style: primaryTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: medium
+            ),),
+            SizedBox(height: 12),
+            Text(
+              'You have never done a transaction',
+              style: secondaryTextStyle,
+            ),
+            SizedBox(height: 20),
+            Container(
+              height: 44,
+              child: TextButton(
+                onPressed: (){},
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 10
+                  ),
+                  backgroundColor: primaryColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12)
+                  ),
+                ),
+                child: Text(
+                  'Explore Store',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Chat Page"),
-      ),
+    return Column(
+      children: [
+        header(),
+        content()
+      ],
     );
   }
 }
