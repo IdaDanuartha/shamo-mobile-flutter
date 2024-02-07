3ba6e3b49a0e3f7e25ad40dfc4c6a2162fd39c65
IdaDanuartha <danuart21@gmail.com>
2024-02-07 20:00:17 +0800

    Code Home Page (Header) - done

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -6,11 +6,57 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: bgColor1,
-      body: Center(
-        child: Text("Home Page"),
-      ),
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: defaultMargin,
+          left: defaultMargin,
+          right: defaultMargin
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Hello, Alex",
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 24,
+                      fontWeight: semiBold
+                    ),
+                  ),
+                  Text(
+                    '@alexkeinn',
+                    style: subtitleTextStyle.copyWith(
+                      fontSize: 16
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              width: 54,
+              height: 54,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/image_profile.png'
+                  )
+                )
+              ),
+            )
+          ],
+        ),
+      );  
+    }
+    
+    return ListView(
+      children: [
+        header(),
+      ],
     );
   }
 }
