5dbfa741fdfa5927fc8b943c382febc0d746e58c
IdaDanuartha <danuart21@gmail.com>
2024-02-07 17:31:28 +0800

    add themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,7 +23,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Image.asset('assets/button_add.png'),
+      body: Center(
+        child: Text('Halo semuanya', style: GoogleFonts.poppins(fontSize: 50),),
+      ),
     );
   }
 }
