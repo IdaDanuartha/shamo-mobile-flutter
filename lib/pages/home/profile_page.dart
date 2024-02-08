5e80fab4db1e1d2ed061b58e77c557fda3cea66b
IdaDanuartha <danuart21@gmail.com>
2024-02-08 11:31:37 +0800

    profile page header done

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -1,14 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_flutter/themes.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
+  Widget header() {
+    return AppBar(
+      backgroundColor: bgColor1,
+      automaticallyImplyLeading: false,
+      elevation: 0,
+      flexibleSpace: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(defaultMargin),
+          child: Row(
+            children: [
+              ClipOval(
+                child: Image.asset(
+                  'assets/image_profile.png',
+                  width: 64,
+                ),
+              ),
+              SizedBox(width: 16),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Hello, Alex",
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 24,
+                        fontWeight: semiBold
+                      ),
+                    ),
+                    Text(
+                      "@alexkeinn",
+                      style: subtitleTextStyle.copyWith(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Image.asset(
+                'assets/button_exit.png',
+                width: 20,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Profile Page"),
-      ),
+    return Column(
+      children: [
+        header()
+      ],
     );
   }
 }
