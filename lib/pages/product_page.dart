5e42ef64deb72aed829405c174041b4933ea089c
IdaDanuartha <danuart21@gmail.com>
2024-02-08 12:26:16 +0800

    add carousel in detail product done

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_flutter/themes.dart';
 
@@ -9,23 +10,43 @@ class ProductPage extends StatefulWidget {
 }
 
 class _ProductPageState extends State<ProductPage> {
+  List images = [
+    'assets/image_shoes.png',
+    'assets/image_shoes.png',
+    'assets/image_shoes.png',
+  ];
+
+  int _currentIndex = 0;
+
+  Widget indicator(int index) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 200),
+      width: _currentIndex == index ? 16 : 4,
+      height: 4,
+      margin: EdgeInsets.symmetric(
+        horizontal: 2
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: _currentIndex == index ? primaryColor : Color(0xffC4C4C4)
+      ),
+    );
+  }
+
   Widget header() {
+    int index = -1;
+
     return Column(
       children: [
         Container(
           margin: EdgeInsets.only(
-            top: 20,
-            left: defaultMargin,
-            right: defaultMargin
-          ),
+              top: 20, left: defaultMargin, right: defaultMargin),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               GestureDetector(
                 onTap: () => Navigator.pop(context),
-                child: Icon(
-                  Icons.chevron_left
-                ),                              
+                child: Icon(Icons.chevron_left),
               ),
               Icon(
                 Icons.shopping_bag,
@@ -33,6 +54,32 @@ class _ProductPageState extends State<ProductPage> {
               )
             ],
           ),
+        ),
+        CarouselSlider(
+          items: images
+              .map((image) => Image.asset(
+                    image,
+                    width: MediaQuery.of(context).size.width,
+                    height: 310,
+                    fit: BoxFit.cover,
+                  ))
+              .toList(),
+          options: CarouselOptions(
+            initialPage: 0,
+            onPageChanged: (index, reason) {
+              setState(() {
+                _currentIndex = index;
+              });
+            }
+          ),
+        ),
+        SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: images.map((e) {
+            index++;
+            return indicator(index);
+          }).toList()
         )
       ],
     );
@@ -41,12 +88,9 @@ class _ProductPageState extends State<ProductPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: bgColor6,
-      body: Column(
-        children: [
-          header()
-        ],
-      )
-    );
+        backgroundColor: bgColor6,
+        body: Column(
+          children: [header()],
+        ));
   }
 }
